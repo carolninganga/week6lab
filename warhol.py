11264c11d213a405866f5fe8e22c6e691594d114
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2021-03-15 15:09:33 -0400

    added images to the folder

---
+++ warhol.py
@@ -0,0 +1,13 @@
+# CS 5001 
+# lab 6 
+# generating images to the window
+
+
+import sys
+import graphicsPlus as gr
+
+#read an image an display it in the window 
+
+def main( argv ):
+
+    # if statement to check if the right amount of arguments such as filenanme have been given
